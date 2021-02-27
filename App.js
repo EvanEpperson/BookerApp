5d6a59aaaf3362a4d69946e849a487b5ddc7a43c
Evan Epperson <evanepperson@Evans-MacBook-Pro-2.local>
2021-02-27 11:56:07 -0600

    made flatlist for all of the props

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Entype from 'react-native-vector-icons/Entypo'
 import Post from './src/components/Post';
 import HomeScreen from './src/screens/Home'
 import feed from './assets/data/feed.js'
+import SearchResultsScreen from './src/screens/SearchResults'
 
 const post1 = feed[0];
 const post2 = feed[1];
@@ -16,12 +17,11 @@ export default function App() {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView>
           {/* <HomeScreen /> */}
-          <Post post={post1} />
+          {/* <Post post={post1} />
           <Post post={post2} />
-          <Post post={post3} />
-        </ScrollView>
+          <Post post={post3} /> */}
+          <SearchResultsScreen />
       </SafeAreaView>
     </>
   );
