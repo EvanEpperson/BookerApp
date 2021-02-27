7bbacdbd57a6f8c5c33157c5c9b42eaa7c186ced
Evan Epperson <evanepperson@Evans-MacBook-Pro-2.local>
2021-02-27 09:51:40 -0600

    finished homescreen button css

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,5 +1,4 @@
-
-import { StyleSheet} from 'react-native'
+import { StyleSheet, Dimensions} from 'react-native'
 
 
 
@@ -33,13 +32,16 @@ const styles = StyleSheet.create({
   },
   searchButton: {
     backgroundColor: "#fff",
-    width: 200,
-    height: 40,
-    borderRadius: 10,
-    marginLeft: 25,
-    marginTop: 25,
+    width: Dimensions.get('screen').width - 20,
+    height: 60,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
     justifyContent: "center",
     alignItems: "center",
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
   },
   searchButtonText: {
     fontSize: 16,
